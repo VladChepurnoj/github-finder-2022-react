ee4df557bf9c9c6bcd7ae6b9afa473b63ea62c00
unknown <chepurnojv@gmail.com>
2022-02-17 12:49:43 +0200

    Alert Context & Reducer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
 
 const Home = () => {
   return (
     <Fragment>
+      <UserSearch />
       <UserResults />
     </Fragment>
   );
